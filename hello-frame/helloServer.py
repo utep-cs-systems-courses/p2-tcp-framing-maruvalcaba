ed1539855e14771da3a5bc8a76d8f6fc69fa0e5f
maruvalcaba <maruvalcaba@miners.utep.edu>
2021-03-01 11:36:33 -0700

    I think I achieved what was requested. Now separate sends appear to be recieved as separate messages.

---
+++ hello-frame/helloServer.py
@@ -0,0 +1,42 @@
+#! /usr/bin/env python3
+
+# Echo server program
+
+import socket, sys, re, os
+sys.path.append("../lib")       # for params
+import params
+
+switchesVarDefaults = (
+    (('-l', '--listenPort') ,'listenPort', 50001),
+    (('-?', '--usage'), "usage", False), # boolean (set if present)
+    )
+
+
+
+progname = "echoserver"
+paramMap = params.parseParams(switchesVarDefaults)
+
+listenPort = paramMap['listenPort']
+listenAddr = ''       # Symbolic name meaning all available interfaces
+
+if paramMap['usage']:
+    params.usage()
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((listenAddr, listenPort))
+s.listen(1)              # allow only one outstanding request
+# s is a factory for connected sockets
+
+def encodeMessage(message):
+    encoding = "{"+message+"}"
+    return encoding.encode()
+
+while True:
+    conn, addr = s.accept() # wait until incoming connection request (and accept it)
+    if os.fork() == 0:      # child becomes server
+        print('Connected by', addr)
+        conn.send(encodeMessage("Hello"))
+        conn.send(encodeMessage("World"))
+        conn.shutdown(socket.SHUT_WR)
+
+
